b77b43fe1dbca798ac6e1e07b8a3281168c1ee44
鞠大宝 <1053950353@qq.com>
2017-12-22 16:20:39 +0800

    T-primes succeed。。。。

--- B/Tprimes.java
+++ B/Tprimes.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -8,16 +9,28 @@ import java.util.Scanner;
  */
 public class Tprimes {
     public static void main(String[] args) {
+
+        int x, y;
+        boolean[] primes = new boolean[1000001];
+        Arrays.fill(primes, true);
+        primes[0] = false;
+        primes[1] = false;
+        for (x = 2; x < 1000001; x++) {
+            for (y = x * 2; y < 1000001; y += x) {
+                primes[y] = false;
+            }
+        }
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Long[] input = new Long[n];
         for (int i = 0; i < n; i++) {
-            input[i] = sc.nextLong();
+            Long temp = sc.nextLong();
+            double sqrt = Math.sqrt((double)temp);
+
+            // 判断平方根double类型 & 强制转成int型后是否相等，再判断获取的平方根是否是质数
+            System.out.println(sqrt == (int)sqrt && primes[(int)sqrt] ? "YES" : "NO");
         }
 
     }
-    private static boolean blowHundred() {
 
-        return false;
-    }
 }
